e8821d296752ab933f2a58dbe1562b6a06aac720
ParkJH106 <jeongha4209@naver.com>
2021-04-20 09:26:12 +0900

    Display compass

---
+++ app/src/main/java/kr/ac/smu/smart0356/compassapp/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.smu.smart0356.compassapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
